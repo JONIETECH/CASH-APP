823952df4b19e50f1c6fa0e951e9d5d1a17b7bf3
Your Fav It Guy Ur <121134451+LegacyOffice@users.noreply.github.com>
2024-06-23 17:48:20 +0300

    Shifted the database to a reusable one as the group

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,4 +1,4 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://qkavmtnyxrevqgnyvrmz.supabase.co';
-  static const supabaseAnonyKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrYXZtdG55eHJldnFnbnl2cm16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3OTgxMDQsImV4cCI6MjAzNDM3NDEwNH0.uYiUDr0JQGh6RlCGoXDUW82dCaubi1rhVwqRcbZhvdE';
+  static const supabaseUrl = 'https://pydgnomyeoowvlssumen.supabase.co';
+  static const supabaseAnonyKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZGdub215ZW9vd3Zsc3N1bWVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkxNTMxMDUsImV4cCI6MjAzNDcyOTEwNX0.9E91RfIBW4jUfGhK2asWHKOa02n0TsqbjHssl-YCudQ';
 }
