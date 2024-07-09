b1572e98448dc766aaff6f98a5662a93746cac22
Amanyire <amanyirecindy@gmail.com>
2024-07-09 22:44:07 +0300

    reviews page

---
+++ lib/features/feedback/presentation/pages/reviews.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class ReviewPage extends StatelessWidget {
+  const ReviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Submit a review',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: FeedbackPage(),
+    );
+  }
+}
+
+class FeedbackPage extends StatefulWidget {
+  @override
+  _FeedbackPageState createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends State<FeedbackPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _feedbackController = TextEditingController();
+
+  @override
+  void dispose() {
+    _feedbackController.dispose();
+    super.dispose();
+  }
+
+  void _submitFeedback() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // You can handle the feedback submission here
+      String feedback = _feedbackController.text;
+      print('Feedback submitted: $feedback');
+
+      // Show a snackbar or dialog to inform the user
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Thank you for your feedback!')),
+      );
+
+      // Clear the form
+      _feedbackController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Feedback'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'We value your feedback!',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _feedbackController,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Your Feedback',
+                  alignLabelWithHint: true,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your feedback';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _submitFeedback,
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
