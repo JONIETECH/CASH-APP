a91a646d204f4a2697991db230190e4d75e77349
audreykahunde <audreykahunde@gmail.com>
2024-07-23 16:34:18 +0300

    added success message

--- lib/features/charity/presentation/charity_donations_viewmodel.dart
+++ lib/features/charity/presentation/charity_donations_viewmodel.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
+//import 'package:path/path.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fl_chart/fl_chart.dart';
 
